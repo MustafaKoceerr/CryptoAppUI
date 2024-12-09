c1b8cc6695959672e7c24a0a6662c32dd5df0b97
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-10 02:51:00 +0300

    initial commit with love <3

---
+++ app/src/main/java/com/kocerlabs/cryptoappmvvm/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.kocerlabs.cryptoappmvvm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.kocerlabs.cryptoappmvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+
+    }
+}
